d471435e34a97e21540072aed349f7f3accc3fcd
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-03 00:41:48 +0530

    declaring an array variable

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
+        int[] array = new int[size];
     }
 }
