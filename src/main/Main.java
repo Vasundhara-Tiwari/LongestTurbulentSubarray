1a45e72da41b967f5cf769071ecaea05a669adfb
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-03 00:41:21 +0530

    taking input for array size

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
     }
 }
