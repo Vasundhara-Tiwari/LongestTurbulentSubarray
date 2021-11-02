c7da350fa8d63970f901ba90a1923624d67c463c
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-03 00:35:43 +0530

    adding solution logic

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -6,6 +6,12 @@ public class Solution {
         int index = 1;
         int maximumLength = 1;
         long lastDifference = 0;
+        while (index < arr.length) {
+            int newDifference = arr[index] - arr[index - 1];
+            if (newDifference * lastDifference > 0) {
+                length = index - 1;
+            } 
+        }
         return maximumLength;
     }
 }
