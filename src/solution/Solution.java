b34d2c878e093f8bd2d69aad86b4753df35e456d
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-03 00:33:26 +0530

    declaring required variables

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -2,6 +2,10 @@ package solution;
 
 public class Solution {
     public int maxTurbulenceSize(int[] arr) {
+        int length = 0;
+        int index = 1;
+        int maximumLength = 1;
+        long lastDifference = 0;
         return 0;
     }
 }
