6ddeff516f24f9cf451a5b630697ecc7b720a2bd
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-05 23:55:05 +0530

    https://leetcode.com/problems/longest-turbulent-subarray/ solution

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,12 +7,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         int size = scanner.nextInt();
         int[] array = new int[size];
+
         for(int i=0; i<size; i++){
             array[i]=scanner.nextInt();
         }
+
         Solution obj = new Solution();
         int result = obj.maxTurbulenceSize(array);
         System.out.println(result);
